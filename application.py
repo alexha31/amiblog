c2253c613de711bff31d983d1fa1b0ce09d39178
Morganx <dianajulissa09@gmail.com>
2021-06-04 23:24:47 -0600

    Cambios

--- application.py
+++ application.py
@@ -26,7 +26,6 @@ Session(app)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-
     if request.method == "POST":
         username = request.form.get("username")
         name = request.form.get("name")
@@ -47,6 +46,7 @@ def register():
             a = db.execute("INSERT INTO usuarios (username, password, nombre, apellido) VALUES (:username,:password,:nombre, :apellido)", username = request.form.get("username"), password = generate_password_hash(password), nombre = name, apellido = lastname)
             print("a")
         session["user_id"] = a
+        
         return redirect("/")
     else:
         return render_template("register.html")
@@ -54,14 +54,13 @@ def register():
 @app.route('/')
 @login_required
 def inicio():
+    flash('Bienvenido', 'success')
     username = db.execute("SELECT username FROM usuarios WHERE ID = :ID", ID = session["user_id"])
-    posts = db.execute("SELECT p.*, u.username FROM post p INNER JOIN usuarios u on u.ID = p.autor")
-    comentarios =db.execute("SELECT c.*, u.username FROM comentarios c INNER JOIN usuarios u on u.ID = c.id_user")
+    posts = db.execute("SELECT p.*, u.username FROM post p INNER JOIN usuario u on u.id_user = p.autor ORDER BY p.id_post DESC")
+    #comentarios =db.execute("SELECT c.*, u.username FROM comentarios c INNER JOIN usuarios u on u.ID = c.id_user")
 
     print(username)
-    return render_template("index.html", username = username[0]["username"], posts = posts, comentarios=comentarios)
-
-
+    return render_template("index.html", username = username[0]["username"], posts = posts)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -72,8 +71,20 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        Error = None
-        ingre = db.execute("SELECT * FROM usuarios WHERE username = :username",username=request.form.get("username"))
+        rows = db.execute("select * from usuario where username = :username", username = username)
+        if len(rows) == 0:
+            return render_template("login.html", error="Usuario no existe")
+        password_db = rows[0]["password"]
+        if not check_password_hash(password_db, password):
+            return render_template("login.html", error="contraseña incorrecta")
+        else:
+            session["id_user"] = rows[0]["id_user"]
+            return redirect("/")
+    else:
+        return render_template("login.html")
+
+        #Error = None
+        '''ingre = db.execute("SELECT * FROM usuarios WHERE username = :username",username=request.form.get("username"))
 
         if len(ingre) != 1:
             flash('Su nombre de usuario es incorrecto', 'error')
@@ -90,12 +101,12 @@ def login():
             session["user_id"] = ingre[0]["ID"]
             return redirect("/")
     else:
-        return render_template("login.html")
+        return render_template("login.html")'''
 
 
-@app.route('/config', methods=["GET", "POST"])
+@app.route('/editarperfil', methods=["GET", "POST"])
 @login_required
-def config():
+def editarperfil():
     if request.method == "POST":
         nombre = request.form.get("name")
         apellido = request.form.get("lastname")
@@ -207,4 +218,4 @@ def salir():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=7000)
